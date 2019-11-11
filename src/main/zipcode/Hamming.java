b64fbc0248d2f66fed93f3c5cebb6be1850f5f6d
Brandon Chambers <brandonmchambers36@gmail.com>
2019-11-10 22:26:12 -0500

    complete

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,32 @@
 package zipcode;
 
+
 public class Hamming {
-    public Hamming(String s, String s1) {
+
+    String s;
+    String s1;
+
+    public Hamming(String s, String s1) throws IllegalArgumentException {
+        if(s.length() != s1.length())
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+
+        this.s = s;
+        this.s1 = s1;
     }
 
-    public int getHammingDistance() {
-        return -1;
+    public int getHammingDistance(){
+
+
+        StringBuilder str1 = new StringBuilder(s);
+        StringBuilder str2 = new StringBuilder(s1);
+        int notEqual = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (str1.charAt(i) != str2.charAt(i)) {
+                notEqual++;
+            }
+        }
+        return notEqual;
+
     }
 }
